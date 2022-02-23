3a53346a22f90fa69734d0f7f2e0f71cf5bb05d8
tachun <creed88@gmail.com>
2022-02-23 13:57:06 +0100

    use dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import './App.css'
 
-import logo from './logo.svg'
 import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Testing iframe</h1>
       <iframe src="https://uatplaylistwidget.smartmusic.com/playlists/474-untitled_-playlist-102____-1645193416" width="400" height="480" frameBorder="0"></iframe>
-    </>
+    </div>
   )
 }
 
