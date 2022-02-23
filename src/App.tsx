f91f060882e19878c497966f0c7b6173b8cb4104
tachun <creed88@gmail.com>
2022-02-23 13:50:43 +0100

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1>Testing iframe</h1>
-      <iframe src="https://uatplaylistwidget.smartmusic.com/playlists/474-untitled_-playlist-102____-1645193416" width="400" height="480" frameborder="0" allowtransparency="true"></iframe>
+      <iframe src="https://uatplaylistwidget.smartmusic.com/playlists/474-untitled_-playlist-102____-1645193416" width="400" height="480" frameBorder="0"></iframe>
     </>
   )
 }
