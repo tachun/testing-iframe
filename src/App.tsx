815ea1475593234212085a29ed1dd4ccb9156389
tachun <creed88@gmail.com>
2022-02-28 18:57:28 +0100

    z

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <h1>Testing iframe</h1>
-      <iframe src="https://uatplaylistwidget.smartmusic.com/playlists/474-untitled_-playlist-102____-1645193416" width="400" height="480" frameBorder="0"></iframe>
+      <iframe src="http://playlist-widget.smartmusic.localhost:4211/playlists/9-my-first-playlist-with-a-name-super-super-mega-long-my-first-playlist-with-a-name-super-super-mega-long-1644944094" width="400" height="480" frameBorder="0"></iframe>
     </div>
   )
 }
